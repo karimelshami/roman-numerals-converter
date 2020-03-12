95dc8417b71a1a007a3253b6a664e4b10b1135a0
Karim El Shami <karim.elshami@vezeeta.com>
2020-03-12 15:39:29 +0200

    remove unnecessary files

--- src/Componants/InputCard/InputCard.js
+++ src/Componants/InputCard/InputCard.js
@@ -3,10 +3,8 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
-import { useStyles } from './InputCard.style'
 
 const InputCard = props => {
-  const classes = useStyles()
   const { helperText, handleChange, placeholderText, type, error } = props
 
   return (
